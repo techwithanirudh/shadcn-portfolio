5622b7b504247b0c87d04a9d44803e178768f56d
srirambalajigit <51313549+srirambalajigit@users.noreply.github.com>
2024-04-02 10:35:34 +0000

    Updated Projects

--- components/sections/Projects/Projects.tsx
+++ components/sections/Projects/Projects.tsx
@@ -22,10 +22,10 @@ function Projects() {
         <div className="grid gap-10 lg:grid-cols-2">
           <div className="space-y-4">
             <h2 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl/none">
-              My Projects
+              My Tools
             </h2>
             <p className="text-gray-500 dark:text-gray-400">
-              Here are some of my featured projects.
+              Here are some of my featured tools that I created.
             </p>
           </div>
           <div className="flex items-center justify-center overflow-hidden lg:max-w-2xl lg:px-12">
@@ -42,9 +42,9 @@ function Projects() {
                   >
                     <div className="p-1">
                       <ProjectCard
-                        thumbnail={"https://www.youtube.com/embed/xiWEX-AvlAk"}
-                        title={`Project A`}
-                        description={`A very interesting project which is  times better than the other one.`}
+                        thumbnail={"https://www.youtube.com/embed/1qrdRJqjGaY"}
+                        title={`Power Platform Developer Copilot`}
+                        description={`Power Platform Developer Copilot is a  tool to assist Power Platform developers. It helps in creating PowerFX expresssions, create FetchXML based on natural language, explain FetchXML, convert SQL to FetchXML, get queryexpresssion for FetchXML, generate Liquid code and create PCF Controls, Dynamics 365 Plugins and custom workflows using a prompt.`}
                       />  
                     </div>
                    
@@ -53,12 +53,81 @@ function Projects() {
                   <div  className="p-1">
                         <ProjectCard
                         thumbnail={"https://www.youtube.com/embed/xiWEX-AvlAk"}
-                        title={`Project A`}
-                        description={`A very interesting project which is  times better than the other one.`}
+                        title={`Power Platform Architect Copilot`}
+                        description={`Power Platform Architect Copilot is a  tool  to assist Power Platform architects. It helps in creating Sequence diagrams, Use Case diagrams, System Context Diagrams. It can also help answer any Power Platform architecture questions based on Power Platform documentation.`}
                       />
                       </div>
                   </CarouselItem>
-                
+                  <CarouselItem>
+                  <div  className="p-1">
+                        <ProjectCard
+                        thumbnail={"https://www.youtube.com/embed/lz2teCAlWog"}
+                        title={`Power Platform Tester Copilot`}
+                        description={`Power Platform Tester Copilot is a  tool I built to assist Power Platform Testers. It helps testers to generate manual test cases for a user story, automated testing using EasyRepro and FakeXrmEasy. It also helps in generating fictitious test data based on the requirement.
+                        `}
+                      />
+                      </div>
+                  </CarouselItem>
+                  <CarouselItem>
+                  <div  className="p-1">
+                        <ProjectCard
+                        thumbnail={"https://www.youtube.com/embed/kR72CRaBchI"}
+                        title={`Power Platform AI Assistants Toolbox`}
+                        description={`Power Platform AI Assistants Toolbox consists of following 18 AI Assistant tools for Power Platform:PowerFX Formula Assistant,PowerFX Explainer,PCF Controls Assistant,Dataverse Data Model Assistant,Test Case Assistant,REST API Automation Assistant,EasyRepro Automation Assistant,FakeXrmEasy  Automation Assistant,FetchXml Assistant,FetchXml to Query Expression Assistant
+                        FetchXml to Dynamics 365 OData Conversion Assistant,FetchXml Explainer,SQL to FetchXml Assistant,Power Pages Liquid Assistant,JSON to C# Assistant,PowerApps and Dynamics 365 Client Script Assistant,Dynamics 365 Plugins and Workflows Assistant and Dataverse REST Builder Assistant.`}
+                      />
+                      </div>
+                  </CarouselItem>
+                     <CarouselItem>
+                  <div  className="p-1">
+                        <ProjectCard
+                        thumbnail={"https://www.youtube.com/embed/VVzhWxz58Bw"}
+                        title={`PCF Controls OpenAI GPT Tool`}
+                        description={`This tool created using OpenAI custom GPT generates PCF controls based on a prompt and also provides guidance on PCF Controls.
+                        Check it out at: https://chat.openai.com/g/g-AxRE2kU2u-pcf-controls-copilot`}
+                      />
+                      </div>
+                  </CarouselItem>
+                  <CarouselItem>
+                  <div  className="p-1">
+                        <ProjectCard
+                        thumbnail={"https://www.youtube.com/embed/HQEWH5_IJ4o"}
+                        title={`PowerApps Plugins and Workflows OpenAI GPT Tool`}
+                        description={`This tool created using  OpenAI custom GPT is a PowerApps and Dynamics 365 coding expert, providing plugin and workflow code.
+                        Check it out at: https://chat.openai.com/g/g-F35onEIMM-powerapps-plugins-and-workflows-copilot`}
+                      />
+                      </div>
+                  </CarouselItem>
+                  <CarouselItem>
+                  <div  className="p-1">
+                        <ProjectCard
+                        thumbnail={"https://www.youtube.com/embed/wQd-8x_ncz0"}
+                        title={`Dataverse Data Model OpenAI GPT Tool`}
+                        description={`This OpenAI GPT tool creates Data Models, ERDs, and Dataverse code to create tables, with clear explanations. Developers can also  paste the table definition generated in Power Apps Copilot to create the tables.
+                        Check it out at: https://chat.openai.com/g/g-3f80TaBjF-dataverse-data-model-copilot`}
+                      />
+                      </div>
+                  </CarouselItem>              
+                  <CarouselItem>
+                  <div  className="p-1">
+                        <ProjectCard
+                        thumbnail={"https://www.youtube.com/embed/pNpba06sLhg"}
+                        title={`Power Platform Help OpenAI GPT Tool`}
+                        description={`This tool created using OpenAI custom GPT is a Power Platform developer assistant with a focus on Microsoft resources and forums.
+                        Check it out at: https://chat.openai.com/g/g-g9QPwkvcF-power-platform-help`}
+                      />
+                      </div>
+                  </CarouselItem>
+                  <CarouselItem>
+                  <div  className="p-1">
+                        <ProjectCard
+                        thumbnail={"https://www.youtube.com/embed/ynpvw37yzkc"}
+                        title={`Automation Toolpak RPA Tool`}
+                        description={`Automation Toolpak is a basic RPA tool that supports Web UI Automation.
+                        Check it out at: https://chat.openai.com/g/g-g9QPwkvcF-power-platform-help`}
+                      />
+                      </div>
+                  </CarouselItem>   
               </CarouselContent>
               <CarouselPrevious />
               <CarouselNext />
