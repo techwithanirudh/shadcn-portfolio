a0c08d14776dc78f0a505ef6c7e6e6da400a08bb
techwithanirudh <techwithanirudh@gmail.com>
2024-04-17 10:26:26 +0000

    revert: :rewind: Remove mf-cursor, as it isn't being used in the project

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import "./cursor.css";
 
 import { ThemeProvider } from "@/components/theme-provider";
 import { Toaster } from "@/components/ui/toaster";
