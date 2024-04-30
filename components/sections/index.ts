2448df6d5d53c01a846e272eefea4ea8dcd71bb4
Anirudh <70373803+techwithanirudh@users.noreply.github.com>
2024-04-30 12:47:46 +0000

    feat: Move header and footer into sections

--- components/sections/index.ts
+++ components/sections/index.ts
@@ -1,8 +1,10 @@
+import Header from "./Header/Header"
 import Hero from "./Hero/Hero";
 import About from "./About/vertical/About";
 import Skills from "./Skills/Skills";
 import Projects from "./Projects/vertical/Projects";
 import Contact from "./Contact/Contact";
 import Testimonials from "./Testimonials/Testimonials";
+import Footer from "./Footer/Footer"
 
-export { Hero, About, Projects, Skills, Testimonials, Contact };
+export { Header, Hero, About, Projects, Skills, Testimonials, Contact, Footer };
