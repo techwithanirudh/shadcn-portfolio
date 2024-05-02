adfde2c95ff7738d16105c46126197682054d1da
Anirudh <70373803+techwithanirudh@users.noreply.github.com>
2024-05-02 16:08:34 +0000

    veryWIP: Add scaffolding for MDX

--- app/project/[slug]/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Page() {
-  return <div>Page</div>;
-}
-
-export default Page;
--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,22 @@
+import remarkGfm from 'remark-gfm'
+import createMDX from '@next/mdx'
+ 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
   experimental: {
     serverActions: {
       allowedOrigins: ["localhost:3000"],
     },
   },
-};
+}
+ 
+// TODO: Add more plugins like rehype & properly use it
+const withMDX = createMDX({
+  options: {
+    remarkPlugins: [remarkGfm],
+    rehypePlugins: [],
+  },
+})
 
-export default nextConfig;
+export default withMDX(nextConfig)
