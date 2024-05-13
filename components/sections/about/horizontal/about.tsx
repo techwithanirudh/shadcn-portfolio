395fbb85ef5cde78ed89448a6f0d04d984987798
Anirudh Sriram <hello@techwithanirudh.com>
2024-05-13 23:10:39 +0530

    fix: format about page

--- components/sections/about/horizontal/about.tsx
+++ components/sections/about/horizontal/about.tsx
@@ -11,7 +11,7 @@ function About() {
     >
       <div className=" px-4 md:px-6">
         <div className="space-y-4">
-        <div className="flex flex-col items-center lg:items-start">
+          <div className="flex flex-col items-center lg:items-start">
             <Reveal>
               <h2 className="text-4xl font-bold uppercase tracking-tighter sm:text-5xl md:text-5xl lg:text-6xl">
                 About
@@ -24,10 +24,10 @@ function About() {
             </Reveal>
           </div>
           <p className="max-w-[700px] text-gray-500 dark:text-gray-400 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
-          I am a passionate and creative Web Developer with a love for
-              beautiful and functional websites. I have experience working with
-              a variety of web technologies and frameworks and I am always eager
-              to learn new things and take on new challenges.
+            I am a passionate and creative Web Developer with a love for
+            beautiful and functional websites. I have experience working with
+            a variety of web technologies and frameworks and I am always eager
+            to learn new things and take on new challenges.
           </p>
         </div>
       </div>
