17e1d3df976e7cf24931b98b487c623f1a99342b
techwithanirudh <techwithanirudh@gmail.com>
2024-05-19 07:25:25 +0000

    fix: add equal spacing for horizontal devices fix: remove console.log for isInView

--- components/sections/about/horizontal/about.tsx
+++ components/sections/about/horizontal/about.tsx
@@ -10,27 +10,25 @@ function About() {
       id="about"
     >
       {/* TODO: Redesign for horizontal */}
-      <div className=" px-4 md:px-6">
-        <div className="space-y-4">
-          <div className="flex flex-col items-center lg:items-start">
-            <Reveal>
-              <h2 className="text-4xl font-bold uppercase tracking-tighter sm:text-5xl md:text-5xl lg:text-6xl">
-                About
-              </h2>
-            </Reveal>
-            <Reveal>
-              <h2 className="text-4xl font-bold uppercase tracking-tighter sm:text-5xl md:text-5xl lg:text-6xl">
-                Me
-              </h2>
-            </Reveal>
-          </div>
-          <p className="max-w-[700px] text-gray-500 dark:text-gray-400 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
-            I am a passionate and creative Web Developer with a love for
-            beautiful and functional websites. I have experience working with a
-            variety of web technologies and frameworks and I am always eager to
-            learn new things and take on new challenges.
-          </p>
+      <div className="space-y-4 px-4 md:px-6 lg:space-y-10">
+        <div className="flex flex-col items-center lg:items-start">
+          <Reveal>
+            <h2 className="text-4xl font-bold uppercase tracking-tighter sm:text-5xl md:text-5xl lg:text-6xl">
+              About
+            </h2>
+          </Reveal>
+          <Reveal>
+            <h2 className="text-4xl font-bold uppercase tracking-tighter sm:text-5xl md:text-5xl lg:text-6xl">
+              Me
+            </h2>
+          </Reveal>
         </div>
+        <p className="mt-6 max-w-[700px] text-gray-500 dark:text-gray-400 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
+          I am a passionate and creative Web Developer with a love for beautiful
+          and functional websites. I have experience working with a variety of
+          web technologies and frameworks and I am always eager to learn new
+          things and take on new challenges.
+        </p>
       </div>
     </MotionWrap>
   );
