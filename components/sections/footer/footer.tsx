1ccff86bfff97af5538712dbee0f4d0ac13aae85
techwithanirudh <techwithanirudh@gmail.com>
2024-05-19 18:04:59 +0000

    style(header): add backdrop blur for header add(footer): -future for copyright info

--- components/sections/footer/footer.tsx
+++ components/sections/footer/footer.tsx
@@ -6,7 +6,7 @@ function Footer() {
   return (
     <footer className="flex w-full shrink-0 flex-col items-center gap-2 border-t border-gray-200 px-4 py-6 dark:border-gray-700 sm:flex-row md:px-6">
       <p className="text-xs text-gray-500 dark:text-gray-400">
-        © 2024 {meta.author.name}. All rights reserved.
+        © 2024-future {meta.author.name}. All rights reserved.
       </p>
       <nav className="flex gap-4 sm:ml-auto sm:gap-6">
         <Link
