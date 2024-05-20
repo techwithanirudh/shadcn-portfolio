a52bd25507b73dd88017e4311fc6ad56d4b6a824
techwithanirudh <techwithanirudh@gmail.com>
2024-05-20 06:02:09 +0000

    refactor: remove absolute position for home page

--- app/page.tsx
+++ app/page.tsx
@@ -16,24 +16,20 @@ import SmoothScroll from '@/components/smooth-scroll';
 export default function Home() {
   return (
     <SmoothScroll>
-      <div className="overflow-hidden">
-        <div className="absolute inset-0 z-50 h-full w-full">
-          <Preloader />
-          <div className="flex min-h-[100dvh] flex-col">
-            <Header />
-            <main className="flex-1">
-              <Hero />
-              <About />
-              <Projects />
-              <Skills />
-              <Testimonials />
-              <Contact />
-              <Footer />
-            </main>
-          </div>
-          <Cursor />
-        </div>
+      <Preloader />
+      <div className="flex min-h-[100dvh] flex-col">
+        <Header />
+        <main className="flex-1">
+          <Hero />
+          <About />
+          <Projects />
+          <Skills />
+          <Testimonials />
+          <Contact />
+          <Footer />
+        </main>
       </div>
+      <Cursor />
     </SmoothScroll>
   );
 }
