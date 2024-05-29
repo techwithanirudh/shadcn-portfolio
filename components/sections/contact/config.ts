e4b6fd19007466c988f66c8af319de8f869418fe
techwithanirudh <techwithanirudh@gmail.com>
2024-05-29 11:29:15 +0000

    feat: Add customizable contact form

---
+++ components/sections/contact/config.ts
@@ -0,0 +1,13 @@
+import { Contact } from '@/types/contact';
+
+const contact: Contact = {
+  email: 'john@example.com',
+  socials: {
+    github: 'https://github.com/',
+    youtube: 'https://youtube.com/',
+    twitter: 'https://twitter.com/',
+    linkedin: 'https://linkedin.com/'
+  }
+};
+
+export { contact };
