244b6ec6185d330383e95d4b00ca48955d7790f8
techwithanirudh <techwithanirudh@gmail.com>
2024-06-14 11:55:11 +0000

    style: use bg-muted for colors

--- components/sections/projects/config.ts
+++ components/sections/projects/config.ts
@@ -30,7 +30,7 @@ const projects: Project[] = [
     thumbnail: '/images/projects/fitness_tracker/cover.jpg'
   },
   {
-    name: 'Eco-friendly Product Marketplace',
+    name: 'Eco-friendly#F5F5F7 Marketplace',
     slug: 'eco_marketplace',
     description:
       'An e-commerce platform dedicated to eco-friendly products where users can buy, sell, and learn about sustainable living.',
