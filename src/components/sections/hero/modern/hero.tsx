1acacfe49ad82c21d30e7b70db6d28dfbb4909f1
Anirudh Sriram <hello@techwithanirudh.com>
2024-08-30 13:46:22 +0000

    feat: px2

--- src/components/sections/hero/modern/hero.tsx
+++ src/components/sections/hero/modern/hero.tsx
@@ -24,10 +24,11 @@ function Hero() {
       className="relative w-full overflow-hidden bg-background/[0.96]"
       ref={container}
     >
-      <div className="relative z-10 md:-mt-28 h-[50rem] md:h-[68rem] md:min-h-[50dvh]">
+      <div className="relative z-10 h-[42.5dvh] md:h-[51.2dvh] md:min-h-[50dvh] xl:h-[61.2dvh]">
         <div className="relative flex h-full flex-col items-center justify-center">
-          <div className="flex items-center justify-center w-full px-4 md:px-[10.625vw]">
-            <h1 className="text-4xl sm:text-5xl font-light md:text-6xl lg:text-7xl xl:text-8xl 2xl:text-9xl">
+          {/* items-center */}
+          <div className="flex w-full items-center justify-center px-4 md:px-6">
+            <h1 className="text-4xl font-light sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl 2xl:text-9xl">
               <RevealText>A developer</RevealText>
               <br />
               {/* className="whitespace-nowrap" */}
@@ -50,7 +51,7 @@ function Hero() {
       </div>
 
       <motion.div
-        className="relative mt-28 aspect-[4/2] w-screen"
+        className="relative aspect-[4/2] w-screen lg:mt-28"
         style={{ y, scale }}
       >
         <Image
