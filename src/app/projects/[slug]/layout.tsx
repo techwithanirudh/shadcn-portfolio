c98825197c8eb5a870d4564f20ea2487c1c6adb2
Anirudh Sriram <hello@techwithanirudh.com>
2024-09-02 09:34:37 +0000

    fix: build

--- src/app/projects/[slug]/layout.tsx
+++ src/app/projects/[slug]/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next';
 
 import { Header } from '@/components/sections';
 
-import Preloader from '@/app/loading';
+import Loader from '@/app/loader';
 import Cursor from '@/components/cursor/cursor';
 import SmoothScroll from '@/components/smooth-scroll';
 
@@ -13,7 +13,7 @@ export default function ProjectLayout({
 }>) {
   return (
     <SmoothScroll>
-      {/* <Preloader /> */}
+      {/* <Loader /> */}
       <div className="flex min-h-[100dvh] flex-col">
         <Header />
         <main className="my-14 flex-1">{children}</main>
