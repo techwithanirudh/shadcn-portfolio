de11d0942f8f88bec90e4f7c71bce51d7448f411
Anirudh Sriram <hello@techwithanirudh.com>
2024-10-08 12:00:06 +0000

    style: use dialog for skill display

--- src/components/sections/skills/config.ts
+++ src/components/sections/skills/config.ts
@@ -1,32 +1,26 @@
 import { Skill } from '@/types/skill';
-import {
-  CodeIcon,
-  DatabaseIcon,
-  LayoutIcon,
-  SmartphoneIcon
-} from 'lucide-react';
 
 const trimLen: number = -1; // 0 is accordion only, -1 is does not restrict the length
 
 const skills: Skill[] = [
   {
     name: 'Web Development',
-    Icon: CodeIcon,
+    thumbnail: '/images/person/3.jpg',
     description: `Web Development involves creating websites and web applications that are both visually appealing and highly functional. It encompasses a variety of technologies, frameworks, and best practices to ensure a seamless user experience and efficient performance.`
   },
   {
     name: 'UI/UX Design',
-    Icon: LayoutIcon,
+    thumbnail: '/images/person/3.jpg',
     description: `UI/UX Design focuses on creating delightful user experiences through visually appealing and highly functional designs. This skill includes a variety of technologies, frameworks, and best practices to enhance user satisfaction and usability.`
   },
   {
     name: 'Database Management',
-    Icon: DatabaseIcon,
+    thumbnail: '/images/person/3.jpg',
     description: `Database Management involves efficiently storing and organizing data. This skill covers a range of technologies, frameworks, and best practices to ensure data integrity, security, and performance.`
   },
   {
     name: 'Mobile Development',
-    Icon: SmartphoneIcon,
+    thumbnail: '/images/person/3.jpg',
     description: `Mobile Development entails crafting apps for smartphones and tablets that are both visually appealing and highly functional. It includes a variety of technologies, frameworks, and best practices to provide a seamless user experience on mobile devices.`
   }
 ];
--- src/components/sections/skills/cozy/skill-card.tsx
+++ src/components/sections/skills/cozy/skill-card.tsx
@@ -1,74 +1,106 @@
-import React from 'react';
-import { CardContent, CardFooter, Card } from '@/components/ui/card';
-import { buttonVariants } from '@/components/ui/button';
-
-import Link from 'next/link';
-import Image from 'next/image';
-import { CodeIcon } from 'lucide-react';
-
+import {
+  Dialog,
+  DialogTrigger,
+  DialogContent,
+  DialogTitle,
+  DialogImage,
+  DialogSubtitle,
+  DialogClose,
+  DialogDescription,
+  DialogContainer
+} from '@/components/motion-primitives/dialog';
+import { PlusIcon } from 'lucide-react';
 import { Skill } from '@/types/skill';
 
+import { trimLen } from '@/components/sections/skills/config';
 import { cn, trimString } from '@/lib/utils';
-
-import {
-  Accordion,
-  AccordionContent,
-  AccordionItem,
-  AccordionTrigger
-} from '@/components/ui/accordion';
-import { Separator } from '@/components/ui/separator';
 
 import { MemoizedReactMarkdown } from '@/components/markdown';
 
 import remarkGfm from 'remark-gfm';
 import remarkMath from 'remark-math';
 
-import { trimLen } from '@/components/sections/skills/config';
-import TextReveal from '@/components/text-reveal';
-
 interface SkillCardProps extends Skill {
   index: number;
   className?: string;
 }
 
-function SkillCard({
+export default function SkillCard({
   name,
   description,
-  Icon,
+  thumbnail,
   index,
   className
 }: SkillCardProps) {
   return (
-    <Card className={cn('bg-muted/40', className)}>
-      <CardContent className="flex flex-col items-start p-6">
-        <AccordionItem value={`acc-${index}`} className="w-full border-none">
-          <div className="flex w-full items-center justify-between">
-            <span className="text-lg font-semibold">({index})</span>
-
-            {trimLen != -1 && (description || '').length > trimLen ? (
-              <AccordionTrigger />
-            ) : (
-              <>{Icon ? <Icon className="h-8 w-8" /> : <CodeIcon />}</>
-            )}
+    <Dialog
+      transition={{
+        type: 'spring',
+        bounce: 0.05,
+        duration: 0.25
+      }}
+    >
+      <DialogTrigger
+        style={{
+          borderRadius: '12px'
+        }}
+        className={cn("flex flex-col overflow-hidden border border-zinc-950/10 bg-white dark:border-zinc-50/10 dark:bg-zinc-900", className)}
+      >
+        {/* <DialogImage
+          src="/eb-27-lamp-edouard-wilfrid-buquet.jpg"
+          alt="A desk lamp designed by Edouard Wilfrid Buquet in 1925. It features a double-arm design and is made from nickel-plated brass, aluminium and varnished wood."
+          className="h-48 w-full object-cover"
+        /> */}
+        <div className="flex flex-grow flex-col items-end justify-between gap-4 p-6">
+          <button
+            type="button"
+            className="relative ml-1 flex h-10 w-10 shrink-0 scale-100 select-none appearance-none items-center justify-center rounded-full border border-zinc-950/10 text-zinc-500 transition-colors hover:bg-zinc-100 hover:text-zinc-800 focus-visible:ring-2 active:scale-[0.98] dark:border-zinc-50/10 dark:bg-zinc-900 dark:text-zinc-500 dark:hover:bg-zinc-800 dark:hover:text-zinc-50 dark:focus-visible:ring-zinc-500"
+            aria-label="Open dialog"
+          >
+            <PlusIcon size={18} />
+          </button>
+          <div className="flex w-full flex-col gap-2">
+            <DialogTitle className="text-3xl font-bold leading-8 tracking-tight text-zinc-950 dark:text-zinc-50">
+              {name}
+            </DialogTitle>
+            <DialogSubtitle className="text-md text-muted-foreground text-zinc-700 dark:text-zinc-400">
+              {trimLen != -1
+                ? trimString(trimLen, description || '')
+                : description}
+            </DialogSubtitle>
           </div>
-          <div className="grid gap-0.5">
-            <h3 className="mt-2 text-2xl font-bold leading-8 tracking-tight">
-              <TextReveal>{name}</TextReveal>
-            </h3>
-            {trimLen != 0 && (
-              <p className="mt-2 text-base text-muted-foreground">
-                <TextReveal>
-                  {trimLen != -1
-                    ? trimString(trimLen, description || '')
-                    : description}
-                </TextReveal>
-              </p>
-            )}
-          </div>
-
-          {trimLen != -1 && (description || '').length > trimLen && (
-            <AccordionContent className="text-md">
-              <Separator className="my-2" />
+        </div>
+      </DialogTrigger>
+      <DialogContainer>
+        <DialogContent
+          style={{
+            borderRadius: '24px'
+          }}
+          className="pointer-events-auto relative flex h-auto w-full flex-col overflow-hidden border border-zinc-950/10 bg-white dark:border-zinc-50/10 dark:bg-zinc-900 sm:w-[500px]"
+        >
+          {thumbnail && (
+            <DialogImage
+              src={thumbnail}
+              alt={`An image which depicts the skill (${name})`}
+              className="h-full w-full"
+            />
+          )}
+          <div className="p-6">
+            <DialogTitle className="text-3xl font-bold leading-8 tracking-tight text-zinc-950 dark:text-zinc-50">
+              {name}
+            </DialogTitle>
+            {/* <DialogSubtitle className="text-zinc-700 dark:text-zinc-400">
+              {description}
+            </DialogSubtitle> */}
+            <DialogDescription
+              className="text-md text-muted-foreground text-zinc-700 dark:text-zinc-400"
+              disableLayoutAnimation
+              variants={{
+                initial: { opacity: 0, scale: 0.8, y: 100 },
+                animate: { opacity: 1, scale: 1, y: 0 },
+                exit: { opacity: 0, scale: 0.8, y: 100 }
+              }}
+            >
               <MemoizedReactMarkdown
                 className="prose min-w-full break-words text-muted-foreground dark:prose-invert prose-p:leading-relaxed prose-pre:p-0"
                 remarkPlugins={[remarkGfm, remarkMath]}
@@ -78,14 +110,13 @@ function SkillCard({
                   }
                 }}
               >
-                {(trimLen != 0 ? '...' : '') + description!.substring(trimLen)}
+                {description}
               </MemoizedReactMarkdown>
-            </AccordionContent>
-          )}
-        </AccordionItem>
-      </CardContent>
-    </Card>
+            </DialogDescription>
+          </div>
+          <DialogClose className="text-zinc-50" />
+        </DialogContent>
+      </DialogContainer>
+    </Dialog>
   );
 }
-
-export default SkillCard;
