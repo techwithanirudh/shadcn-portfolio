64e16c037f9163ce5a978848a8596f882029ac53
Anirudh Sriram <hello@techwithanirudh.com>
2024-10-13 16:50:39 +0000

    fix: images for skills

--- src/components/sections/skills/config.ts
+++ src/components/sections/skills/config.ts
@@ -6,22 +6,22 @@ const skills: Skill[] = [
   {
     name: 'Web Development',
     // todo: use proper thumbnail pics
-    thumbnail: '/images/person/3.jpg',
+    thumbnail: '/images/skills/web-development.jpg',
     description: `Web Development involves creating websites and web applications that are both visually appealing and highly functional. It encompasses a variety of technologies, frameworks, and best practices to ensure a seamless user experience and efficient performance.`
   },
   {
     name: 'UI/UX Design',
-    thumbnail: '/images/person/3.jpg',
+    thumbnail: '/images/skills/ui-ux-design.jpg',
     description: `UI/UX Design focuses on creating delightful user experiences through visually appealing and highly functional designs. This skill includes a variety of technologies, frameworks, and best practices to enhance user satisfaction and usability.`
   },
   {
     name: 'Database Management',
-    thumbnail: '/images/person/3.jpg',
+    thumbnail: '/images/skills/database-management.jpg',
     description: `Database Management involves efficiently storing and organizing data. This skill covers a range of technologies, frameworks, and best practices to ensure data integrity, security, and performance.`
   },
   {
     name: 'Mobile Development',
-    thumbnail: '/images/person/3.jpg',
+    thumbnail: '/images/skills/mobile-development.jpg',
     description: `Mobile Development entails crafting apps for smartphones and tablets that are both visually appealing and highly functional. It includes a variety of technologies, frameworks, and best practices to provide a seamless user experience on mobile devices.`
   }
 ];
