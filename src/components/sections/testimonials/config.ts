f04351147fbe07fca309c0f3d94d61637a879a37
Anirudh Sriram <hello@techwithanirudh.com>
2024-10-16 09:59:11 +0000

    fix: add images based on the name

--- src/components/sections/testimonials/config.ts
+++ src/components/sections/testimonials/config.ts
@@ -3,173 +3,172 @@ import { Testimonial } from '@/types/testimonial';
 const testimonials: Testimonial[] = [
   {
     name: 'Jane Doe',
-    image: '/images/person/1.jpg',
+    image: '/images/person/janedoe.jpg',
     username: 'janedoe',
     testimonial:
       'Collaborating with John on this project was a fantastic experience. His dedication and expertise were invaluable.'
   },
   {
     name: 'Emily Smith',
-    image: '/images/person/2.jpg',
+    image: '/images/person/emilysmith.jpg',
     username: 'emilysmith',
     testimonial:
       "John's work on the project was outstanding. His problem-solving skills and attention to detail greatly contributed to our success."
   },
   {
     name: 'Michael Johnson',
-    image: '/images/person/3.jpg',
+    image: '/images/person/michaeljohnson.jpg',
     username: 'michaeljohnson',
     testimonial:
       "John's innovative approach and technical proficiency were key to the success of our project. Highly recommend working with him."
   },
   {
     name: 'Sarah Williams',
-    image: '/images/person/4.jpg',
+    image: '/images/person/sarahwilliams.jpg',
     username: 'sarahwilliams',
     testimonial:
       'John has an exceptional ability to communicate complex ideas clearly. His contributions were crucial to the project’s success.'
   },
   {
     name: 'David Brown',
-    image: '/images/person/5.jpg',
+    image: '/images/person/davidbrown.jpg',
     username: 'davidbrown',
     testimonial:
       'Working with John was a pleasure. His professionalism and technical skills are top-notch. Highly recommend him for any project.'
   },
   {
     name: 'Jessica Taylor',
-    image: '/images/person/6.jpg',
+    image: '/images/person/jessicataylor.jpg',
     username: 'jessicataylor',
     testimonial:
       "John's creativity and problem-solving abilities were impressive. He played a key role in the project's success."
   },
   {
     name: 'Oliver Martinez',
-    image: '/images/person/7.jpg',
+    image: '/images/person/olivermartinez.jpg',
     username: 'olivermartinez',
     testimonial:
       "John's technical leadership and clear communication made a big difference in our project's outcome."
   },
   {
     name: 'Sophia Turner',
-    image: '/images/person/8.jpg',
+    image: '/images/person/sophiaturner.jpg',
     username: 'sophiaturner',
     testimonial:
       'John’s expertise in software development is second to none. He was a vital asset in our team’s success.'
   },
   {
     name: 'Noah Scott',
-    image: '/images/person/9.jpg',
+    image: '/images/person/noahscott.jpg',
     username: 'noahscott',
     testimonial:
       'John’s ability to troubleshoot and resolve issues quickly was critical to our project staying on track.'
   },
   {
     name: 'Lily Johnson',
-    image: '/images/person/10.jpg',
+    image: '/images/person/lilyjohnson.jpg',
     username: 'lilyjohnson',
     testimonial:
       'John’s guidance and attention to detail greatly enhanced the quality of our project. He’s a pleasure to work with.'
   },
   {
     name: 'James White',
-    image: '/images/person/11.jpg',
+    image: '/images/person/jameswhite.jpg', // todo
     username: 'jameswhite',
     testimonial:
       "John’s technical prowess and collaborative spirit made him an indispensable part of the project."
   },
   {
     name: 'Isabella Evans',
-    image: '/images/person/12.jpg',
+    image: '/images/person/isabellaevans.jpg',
     username: 'isabellaevans',
     testimonial:
       "John's contributions were instrumental to our project’s success. His work ethic and creativity were outstanding."
   },
   {
     name: 'Henry Adams',
-    image: '/images/person/13.jpg',
+    image: '/images/person/henryadams.jpg',
     username: 'henryadams',
     testimonial:
       'John’s professionalism and dedication were crucial in delivering the project on time and with high quality.'
   },
   {
     name: 'Ava Davis',
-    image: '/images/person/14.jpg',
+    image: '/images/person/avadavis.jpg',
     username: 'avadavis',
     testimonial:
       'John’s ability to lead complex projects with ease made the difference in achieving our goals.'
   },
   {
     name: 'William Harris',
-    image: '/images/person/15.jpg',
+    image: '/images/person/williamharris.jpg',
     username: 'williamharris',
     testimonial:
       "John’s thoroughness and problem-solving skills ensured the project’s success. I highly recommend working with him."
   },
   {
     name: 'Mia Clark',
-    image: '/images/person/16.jpg',
+    image: '/images/person/miaclark.jpg',
     username: 'miaclark',
     testimonial:
       'John is an outstanding collaborator and technical expert. His input significantly improved our project outcomes.'
   },
   {
     name: 'Alexander Lewis',
-    image: '/images/person/17.jpg',
+    image: '/images/person/alexanderlewis.jpg',
     username: 'alexanderlewis',
     testimonial:
       'John’s dedication and expertise were instrumental in overcoming the challenges we faced during the project.'
   },
   {
     name: 'Charlotte Young',
-    image: '/images/person/18.jpg',
+    image: '/images/person/charlotteyoung.jpg', // todo
     username: 'charlotteyoung',
     testimonial:
       'John’s creative thinking and attention to detail played a significant role in our project’s success.'
   },
   {
     name: 'Liam Walker',
-    image: '/images/person/19.jpg',
+    image: '/images/person/liamwalker.jpg',
     username: 'liamwalker',
     testimonial:
       "John's collaborative nature and technical knowledge made him an invaluable team member."
   },
   {
     name: 'Amelia Hall',
-    image: '/images/person/20.jpg',
+    image: '/images/person/ameliahall.jpg',
     username: 'ameliahall',
     testimonial:
       'John’s strategic thinking and ability to solve complex problems were key to the success of our project.'
   },
   {
     name: 'Ethan King',
-    image: '/images/person/21.jpg',
+    image: '/images/person/ethanking.jpg',
     username: 'ethanking',
     testimonial:
       'John’s technical expertise and dedication ensured that we delivered a high-quality project on time.'
   },
   {
     name: 'Olivia Wright',
-    image: '/images/person/22.jpg',
+    image: '/images/person/oliviawright.jpg',
     username: 'oliviawright',
     testimonial:
       'John is an exceptional problem-solver and communicator. He consistently delivered top-notch work on the project.'
   },
   {
     name: 'Benjamin Hill',
-    image: '/images/person/23.jpg',
+    image: '/images/person/benjaminhill.jpg',
     username: 'benjaminhill',
     testimonial:
       'John’s leadership and technical knowledge were instrumental in navigating the complexities of our project.'
   },
   {
     name: 'Eleanor Scott',
-    image: '/images/person/24.jpg',
+    image: '/images/person/eleanorscott.jpg',
     username: 'eleanorscott',
     testimonial:
       "John’s ability to bring clarity and structure to the project was invaluable. He’s a pleasure to work with."
   }
-  // todo: add 4 more testimonials
   // todo: add attribution for portfolio unsplash images
 ];
 
