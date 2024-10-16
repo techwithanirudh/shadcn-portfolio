48b12a862269c575e120e1eb4fd0ea8bdd660753
Anirudh Sriram <hello@techwithanirudh.com>
2024-10-16 10:59:49 +0000

    fix: testimonial-card

--- src/components/sections/testimonials/modern/testimonial-card.tsx
+++ src/components/sections/testimonials/modern/testimonial-card.tsx
@@ -19,7 +19,7 @@ function TestimonialCard({
   return (
     <Card className={cn('h-full w-full rounded-xl', 'bg-muted/40 hover:bg-muted', className)}>
       <div className="flex items-center p-4 gap-3">
-        <div className="h-16 w-16 overflow-hidden rounded-md border-2 border-white">
+        <div className="h-12 w-12 xl:h-16 xl:w-16 overflow-hidden rounded-md border border-border">
           <Image
             src={image || '/placeholder.svg'}
             alt={name || 'Anonymous'}
@@ -29,12 +29,12 @@ function TestimonialCard({
           />
         </div>
         <div>
-          <p className="font-semibold text-lg">{name || 'Anonymous'}</p>
-          {username && <p className="text-md text-gray-500">@{username}</p>}
+          <p className="font-semibold xl:text-lg">{name || 'Anonymous'}</p>
+          {username && <p className="text-sm lg:text-md text-gray-500">@{username}</p>}
         </div>
       </div>
       <CardContent className="p-4">
-        <p className="text-sm leading-loose lg:text-lg">
+        <p className="text-sm leading-loose xl:text-lg">
           {testimonial || 'No testimonial provided.'}
         </p>
       </CardContent>
