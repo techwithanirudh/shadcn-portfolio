35ee0bee8de2e6ce26c512f19eb90f1083f7a28b
Anirudh Sriram <hello@techwithanirudh.com>
2024-12-07 14:32:36 +0000

    fix: mobile responsiveness issues

--- src/components/sections/footer/modern/content.tsx
+++ src/components/sections/footer/modern/content.tsx
@@ -4,6 +4,7 @@ import { metadata as meta } from '@/app/config';
 
 import { footer } from '@/components/sections/footer/config';
 import { links } from '@/components/sections/header/config';
+import { contact } from '@/components/sections/contact/config';
 
 export default function Content() {
     return (
@@ -16,9 +17,9 @@ export default function Content() {
 
 const Copyright = () => {
     return (
-        <div className='flex justify-between items-end'>
-            <h1 className='text-[22vw] leading-[0.8] mt-10'>Portfolio</h1>
-            <p className='text-sm'>© 2024 {meta.author.name}</p>
+        <div className='flex flex-col sm:flex-row justify-between items-start sm:items-end'>
+            <h1 className='text-[18vw] md:text-[16vw] lg:text-[18vw] xl:text-[20vw] 2xl:text-[22vw] leading-[0.8] mt-10'>Portfolio</h1>
+            <p className='text-xs sm:text-sm md:text-base lg:text-lg xl:text-xl 2xl:text-2xl mt-4 sm:mt-0'>© 2024 {meta.author.name}</p>
         </div>
     )
 }
@@ -38,6 +39,24 @@ const Nav = () => {
                             key={`ft-l_about_${index}`}
                         >
                             {title}
+                        </Link>
+                    );
+                })}
+            </div>
+            <div className='flex flex-col gap-2'>
+                <h3 className='mb-2 uppercase text-zinc-500 dark:text-zinc-400'>Socials</h3>
+                {contact.socials.map((link, index) => {
+                    const { name, href } = link;
+
+                    return (
+                        <Link
+                            className="underline-offset-4 hover:underline"
+                            href={href}
+                            target='_blank'
+                            key={`ft-l_social_${index}`}
+                            external
+                        >
+                            {name}
                         </Link>
                     );
                 })}
--- src/components/sections/footer/modern/footer.tsx
+++ src/components/sections/footer/modern/footer.tsx
@@ -5,10 +5,10 @@ export default function Footer() {
   return (
     <>
       <div
-        className='relative h-[800px]'
+        className="relative h-[400px] md:h-[500px] lg:h-[600px] xl:h-[700px] 2xl:h-[800px]"
         style={{ clipPath: "polygon(0% 0, 100% 0%, 100% 100%, 0 100%)" }}
       >
-        <div className='fixed bottom-0 h-[800px] w-full'>
+        <div className='fixed bottom-0  h-[400px] md:h-[500px] lg:h-[600px] xl:h-[700px] 2xl:h-[800px] w-full'>
           <Content />
         </div>
       </div>
