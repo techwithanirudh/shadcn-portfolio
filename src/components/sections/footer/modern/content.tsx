eb3c9b9905b575e997d3eaa3cb920b81930ffe82
Anirudh Sriram <hello@techwithanirudh.com>
2024-12-07 13:15:09 +0000

    style: update footer design

--- src/components/sections/footer/modern/content.tsx
+++ src/components/sections/footer/modern/content.tsx
@@ -1,18 +1,63 @@
 import React from 'react'
+import Link from '@/components/motion/link';
+import { metadata as meta } from '@/app/config';
+
+import { footer } from '@/components/sections/footer/config';
+import { links } from '@/components/sections/header/config';
 
 export default function Content() {
     return (
         <div className='bg-muted/30 py-8 px-12 h-full w-full flex flex-col justify-between'>
-            <Section2 />
+            <Nav />
+            <Copyright />
         </div>
     )
 }
 
-const Section2 = () => {
+const Copyright = () => {
     return (
         <div className='flex justify-between items-end'>
             <h1 className='text-[22vw] leading-[0.8] mt-10'>Portfolio</h1>
-            <p>©copyright</p>
+            <p className='text-sm'>© 2024 {meta.author.name}</p>
+        </div>
+    )
+}
+
+const Nav = () => {
+    return (
+        <div className='flex shrink-0 gap-20'>
+            <div className='flex flex-col gap-2'>
+                <h3 className='mb-2 uppercase text-zinc-500 dark:text-zinc-400'>About</h3>
+                {links.map((link, index) => {
+                    const { title, href } = link;
+
+                    return (
+                        <Link
+                            className="underline-offset-4 hover:underline"
+                            href={href}
+                            key={`ft-l_about_${index}`}
+                        >
+                            {title}
+                        </Link>
+                    );
+                })}
+            </div>
+            <div className='flex flex-col gap-2'>
+                <h3 className='mb-2 uppercase text-zinc-500 dark:text-zinc-400'>More</h3>
+                {footer.map((link, index) => {
+                    const { title, href } = link;
+
+                    return (
+                        <Link
+                            className="underline-offset-4 hover:underline"
+                            href={href}
+                            key={`ft-l_more_${index}`}
+                        >
+                            {title}
+                        </Link>
+                    );
+                })}
+            </div>
         </div>
     )
 }
--- src/components/sections/footer/modern/footer.tsx
+++ src/components/sections/footer/modern/footer.tsx
@@ -5,10 +5,10 @@ export default function Footer() {
   return (
     <>
       <div
-        className='relative h-[900px]'
+        className='relative h-[800px]'
         style={{ clipPath: "polygon(0% 0, 100% 0%, 100% 100%, 0 100%)" }}
       >
-        <div className='fixed bottom-0 h-[900px] w-full'>
+        <div className='fixed bottom-0 h-[800px] w-full'>
           <Content />
         </div>
       </div>
