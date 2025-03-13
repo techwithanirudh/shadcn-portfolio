145cdd08a7250f20e00d85218ee6b1f8162da12a
Anirudh Sriram <hello@techwithanirudh.com>
2025-03-13 17:30:02 +0000

    fix: pnpm i

---
+++ packages/db/drizzle.config.ts
@@ -0,0 +1,14 @@
+import type { Config } from "drizzle-kit";
+
+if (!process.env.POSTGRES_URL) {
+  throw new Error("Missing POSTGRES_URL");
+}
+
+const nonPoolingUrl = process.env.POSTGRES_URL.replace(":6543", ":5432");
+
+export default {
+  schema: "./src/schema.ts",
+  dialect: "postgresql",
+  dbCredentials: { url: nonPoolingUrl },
+  casing: "snake_case",
+} satisfies Config;
---
+++ packages/db/src/client.ts
@@ -0,0 +1,10 @@
+import { sql } from "@vercel/postgres";
+import { drizzle } from "drizzle-orm/vercel-postgres";
+
+import * as schema from "./schema";
+
+export const db = drizzle({
+  client: sql,
+  schema,
+  casing: "snake_case",
+});
