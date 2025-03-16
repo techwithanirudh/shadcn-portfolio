a7b0a7888851c94bc3c657b5665b025b32003148
Anirudh Sriram <hello@techwithanirudh.com>
2025-03-16 08:58:15 +0000

    fix: use DATABASE_URL

--- packages/db/src/client.ts
+++ packages/db/src/client.ts
@@ -4,7 +4,7 @@ import { drizzle } from "drizzle-orm/neon-http";
 import * as schema from "./schema";
 
 if (!process.env.DATABASE_URL) {
-  throw new Error("Missing POSTGRES_URL");
+  throw new Error("Missing DATABASE_URL");
 }
 
 const sql = neon(process.env.DATABASE_URL);
